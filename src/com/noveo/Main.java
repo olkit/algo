7011a79d77f01cc2ed809685670e57ab628df725
olki <olki92@gmail.com>
2022-09-29 14:56:17 +0600

    add Task11

--- src/com/noveo/Main.java
+++ src/com/noveo/Main.java
@@ -1,5 +1,6 @@
 package com.noveo;
 
+import com.noveo.array.Task11;
 import com.noveo.binarysearch.Task6;
 import com.noveo.binarysearch.Task7;
 import com.noveo.binarysearchtree.Task3;
@@ -13,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Task task = new Task7();
+        Task task = new Task11();
         task.run();
     }
 }
--- src/com/noveo/array/Task11.java
+++ src/com/noveo/array/Task11.java
@@ -9,5 +9,31 @@ import com.noveo.model.TreeNode;
 public class Task11 implements Task {
 
     public void run() {
+        int[] arr = new int[]{2,0,2,1,1,0};
+        sortColors(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    public void sortColors(int[] nums) {
+
+        final int RED_NUMBER = 0;
+        final int WHITE_NUMBER = 1;
+        final int BLUE_NUMBER = 2;
+
+        int countRed = 0;
+        int countWhite = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == RED_NUMBER) countRed++;
+            else if(nums[i] == WHITE_NUMBER) countWhite++;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(i < countRed) nums[i] = RED_NUMBER;
+            else if(i < countRed + countWhite) nums[i] = WHITE_NUMBER;
+            else nums[i] = BLUE_NUMBER;
+        }
     }
 }
